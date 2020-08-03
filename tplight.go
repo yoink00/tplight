4418d17a012231d37de2ccdf897210034214eb1a
Stuart Wallace <stuart@wapbot.co.uk>
2020-08-03 20:55:50 +0100

    Remove panics and pass errors up the stack

--- tplight.go
+++ tplight.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Bulb interface {
-	SetHSB(hue int, saturation int, brightness int)
-	On()
-	Off()
-	Info() *map[string]int
+	SetHSB(hue int, saturation int, brightness int) error
+	On() error
+	Off() error
+	Info() (*map[string]int, error)
 }
 
 type ldata struct {
@@ -22,7 +22,7 @@ func NewBulb(host string) Bulb {
 	return &ldata{address: host}
 }
 
-func (b ldata) SetHSB(hue, saturation, brightness int) {
+func (b ldata) SetHSB(hue, saturation, brightness int) error {
 	message := []byte("{\"smartlife.iot.smartbulb.lightingservice\":" +
 		"{\"transition_light_state\":" +
 		"{\"ignore_default\":1," +
@@ -33,10 +33,11 @@ func (b ldata) SetHSB(hue, saturation, brightness int) {
 		"\"brightness\":" + strconv.Itoa(brightness) + "," +
 		"\"color_temp\" : 0" +
 		"}}}")
-	send(b.address, message)
+	_, err := send(b.address, message)
+	return err
 }
 
-func (b ldata) SetHSBT(hue, saturation, brightness, transition_period int) {
+func (b ldata) SetHSBT(hue, saturation, brightness, transition_period int) error {
 	message := []byte("{\"smartlife.iot.smartbulb.lightingservice\":" +
 		"{\"transition_light_state\":" +
 		"{\"ignore_default\":1," +
@@ -47,31 +48,37 @@ func (b ldata) SetHSBT(hue, saturation, brightness, transition_period int) {
 		"\"brightness\":" + strconv.Itoa(brightness) + "," +
 		"\"color_temp\" : 0" +
 		"}}}")
-	send(b.address, message)
+	_, err := send(b.address, message)
+	return err
 }
 
-func (b ldata) On() {
+func (b ldata) On() error {
 	message := []byte("{\"smartlife.iot.smartbulb.lightingservice\":" +
 		"{\"transition_light_state\":" +
 		"{\"ignore_default\":1," +
 		"\"on_off\":1" +
 		"}}}")
-	send(b.address, message)
+	_, err := send(b.address, message)
+	return err
 }
 
-func (b ldata) Off() {
+func (b ldata) Off() error {
 	message := []byte("{\"smartlife.iot.smartbulb.lightingservice\":" +
 		"{\"transition_light_state\":" +
 		"{\"ignore_default\":1," +
 		"\"on_off\":0," +
 		"\"transition_period\":2" +
 		"}}}")
-	send(b.address, message)
+	_, err := send(b.address, message)
+	return err
 }
 
-func (b ldata) Info() *map[string]int {
+func (b ldata) Info() (*map[string]int, error) {
 	info := make(map[string]int)
-	parsed := send(b.address, []byte("{\"system\" : {\"get_sysinfo\": {}}}")[:])
+	parsed, err := send(b.address, []byte("{\"system\" : {\"get_sysinfo\": {}}}")[:])
+	if err != nil {
+		return nil, err
+	}
 	data := parsed["system"].(map[string]interface{})["get_sysinfo"].(map[string]interface{})["light_state"].(map[string]interface{})
 	info["onOff"] = int(data["on_off"].(float64))
 
@@ -85,7 +92,7 @@ func (b ldata) Info() *map[string]int {
 		info["brightness"] = int(data["brightness"].(float64))
 	}
 
-	return &info
+	return &info, nil
 
 }
 
@@ -109,27 +116,28 @@ func decrypt(data []byte) (output []byte) {
 	return output
 }
 
-func send(hostname string, message []byte) (parsed map[string]interface{}) {
+func send(hostname string, message []byte) (map[string]interface{}, error) {
 	data := encrypt(message)
 	port := 9999
 	conn, err := net.Dial("udp4", hostname+":"+strconv.Itoa(port))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer conn.Close()
 	_, err = conn.Write(data)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	rData := make([]byte, 1500)
 	rLen, err := bufio.NewReader(conn).Read(rData)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	dData := decrypt(rData[:rLen])
+	var parsed map[string]interface{}
 	err = json.Unmarshal(dData, &parsed)
 	if err != nil {
 		panic(err)
 	}
-	return parsed
+	return parsed, nil
 }
